60eb1b27e6ffb3e7ee227a544a8cfef0070ee679
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2019-10-01 18:23:13 +1000

    rewrite to find two barcodes: process yaml input

--- src/barcodes.pl
+++ src/barcodes.pl
@@ -1,3 +1,9 @@
+# script to count barcodes in NGS data
+# each read may contain up to two barcodes at defined locations within the read
+# each set of barcodes is specified by a yaml file, and its start coordinate within the read must be given
+# output file with counts for each barcode (or combination of barcodes)
+# allow for mismatches and variability in barcode location
+
 #!/usr/bin/perl
 # It's not a bug, it's a feature
 
@@ -5,53 +11,98 @@ use strict;
 use warnings;
 
 use Getopt::Long;
+use YAML::Tiny 'LoadFile';
+use Data::Dumper;
 
-my $barcodes; #input barcodes
+# allocate
+my $barcodes; # barcodes (yml file)
 my $reads; # reads
 my $out = "counts.txt"; #output file for counts
-my $start; #1-based start coordinate of barcode
-my $length; #1-based stop coordinate of barcode
-my $fwdPrimer; #forward primer used for amplicon sequencing
+my $start; #1-based start coordinate of barcode (if only one)
+my $start1; #1-based start coordinate of second barcode
+my $start2; #1-based start coordinate of second barcode
+my $mismatches; #number of allowed mismatches
+my $search; # search this number of bases either side from the expected position of the barcode
 my $toprint = 500000; #for updates every $toPrint reads
 
 my $help;
 
-GetOptions('barcodes=s' 	=> \$barcodes,
+# get inputs from command line
+GetOptions('barcodes=s' 		=> \$barcodes,
 		   'reads=s' 		=> \$reads,
 		   'out=s'  		=> \$out,
 		   'start=i'  		=> \$start,
-		   'length=i' 		=> \$length,
-		   'fwdPrimer=s'  	=> \$fwdPrimer,
+		   'start1=i'  		=> \$start1,
+		   'start2=i'  		=> \$start2,
+		   'mismatches=i'	=> \$mismatches,
+		   'search=i'		=> \$search,
 	       'help',    		=> \$help);
 
 if ($help) { printHelp(); }
 
-unless ($barcodes and $reads and $fwdPrimer and $start and $length) { printHelp(); }
+# check for required inputs
+unless ($barcodes and $reads and ($start1 or $start)) { printHelp(); }
 
-## first read barcode file
-## create an array of barcode sequences
-## also two hashes: one with sequences as keys and names as values, and one with name as key and count as value
+if ($start1) { unless ($start2) { printHelp(); } } 
 
-my %counts; #to store sequence-count pairs
-my %lookup; #to store sequence-name pairs
+#----------------------------------------------------------------------------------------------------------------------------------------
+### Read barcode yaml(s) ###
+#----------------------------------------------------------------------------------------------------------------------------------------
 
-#add "none" to %counts to keep track of reads where there was no match
-$counts{"none"} = 0;
+## barcode yaml(s) should contain a 'dictionary' of barcode names and sequences for each set of barcodes
+##
+## example yaml
+##
+##  barcodes1:
+## 		bc1: ATCTAT
+## 		bc2: TCTGAA
+## 		bc3: TTAGGA
+##  barcodes2:
+##		bc4: GTACA
+##		bc5: GTGAG
+## 	
 
-print "reading barcode file...\n";
+## read yaml files 
+#load data from yaml into hashes (one for each set of barcodes)
+my $barcs_yaml;
+my %barcs1;
+my %barcs2;
+my $barcodes_name;
 
-open(BARC, $barcodes) || die "Could not open barcode file: $barcodes\n";
-while (my $line = <BARC>) {
+# load yaml
+$barcs_yaml = LoadFile($barcodes);
 
-	my @parts = split(" ", $line); #get fields from line (ID, seq, name)
-	
-	if ($parts[2]) { #if barcode has a name
-	
-		$counts{$parts[1]} = 0; #start count at 0 (seq : count)
-		$lookup{$parts[1]} = $parts[2]; #add to lookup table (seq : name)
-	}
+#check number of sets of barcodes in yaml
+if (scalar keys %{$barcs_yaml} > 2) { die "found too many sets of barcodes in yaml"; }
+if (scalar keys %{$barcs_yaml} == 0) { die "didn't find any barcodes in yaml"; }
+
+# get first set of barcodes
+$barcodes_name = (keys %{$barcs_yaml})[0];
+print "loading barcode set 1: $barcodes_name\n";
+%barcs1 = %{ $barcs_yaml->{$barcodes_name} };
+
+# if second set of barcodes, get these too
+if (scalar keys %{$barcs_yaml} > 1) { $barcodes_name = (keys %{$barcs_yaml})[1]; }
+print "loading barcode set 2: $barcodes_name\n";
+%barcs2 = %{ $barcs_yaml->{$barcodes_name} };
+
+# check that number of starts specified corresponds with number of sets of barcodes in input file
+if ($barcs_yaml->{barcodes2}) { 
+	unless ($start1 and $start2) { 
+		print "Specify --start1 and --start2 for two sets of barcodes\n"; 
+	} 	
+} 
+if ($barcs_yaml->{barcodes1}) { 
+	unless ($start) { 
+		die "Specify --start for one set of barcodes\n "; 
+	} 
 }
-close(BARC);
+
+# construct data structure to store counts of barcodes
+# if only one set of barcodes, this is a hash with barcode sequences as keys and count as values
+# if two sets of barcodes, this is a 
+
+
 
 #status update
 print "finding barcodes in reads...\n";
@@ -71,10 +122,10 @@ while (my $line = <READS>) {
 	unless ((($n_lines - 2) % 4) == 0) { next; } 
 	
 	#do we need to reverse-complement?
-	unless ($line =~ /$fwdPrimer/i) { $line = reverseComp($line); }
+	#unless ($line =~ /$fwdPrimer/i) { $line = reverseComp($line); }
 	
 	#get sub-string from start to stop
-	$query = substr($line, $start, $length);  #get barcode from read
+	#$query = substr($line, $start, $length);  #get barcode from read
 	
 	#increment barcode counter
 	if (exists $counts{$query}) { $counts{$query} += 1; } #if matched (assumes one unique match)
@@ -115,13 +166,13 @@ exit;
 
 sub printHelp {
 	print "\nScript for counting barcodes in reads\n\n";
+	
 	print "Usage:\n";
 	print "\tperl barcodes.pl --barcodes <txt> --reads <fastq> --out <txt> --fwdPrimer <seq> --start <num> --length <num> --help\n\n";
 	print "Arguments:\n";
-	print "\t--barcodes:   File with barcodes (txt). First column contains barcode ID, second contains sequence, third contains name\n";
+	print "\t--barcodes:   Yaml file containing one or two sets of barcodes\n";
 	print "\t--reads:  Fastq file containing reads\n";
 	print "\t--out:  Output files for counts of each barcode (txt) [counts.txt]\n";
-	print "\t--fwdPrimer:  1-based stop coordinate of barcode [end of read]\n";
 	print "\t--start:  1-based start coordinate of barcode [1]\n";
 	print "\t--length:  length of barcode [length of read]\n";
 	print "\t--help:    Print this help message and quit\n";
