13b86da6770efea4ac6b9d431a67ae46f1ada00f
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2020-07-17 13:02:08 +1000

    also need to change location of python script in snakefile for docker container

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -7,8 +7,9 @@ cp ../src/barcodes.py .
 cp ../Snakefile .
 # be a bit more restrictive about where data and results are, to make sure
 # that they're always in the directory in which the singularity sif file is
-perl -pe "s/config\[wildcards.sample\]\[\'path\'\]/\"data\/\"/ig" Snakefile > Snakefile.bak
-mv Snakefile.bak Snakefile
+perl -pe "s/config\[wildcards.sample\]\[\'path\'\]/\'data\/\'/g" Snakefile > Snakefile.bak
+perl -pe "s/src/\/src/g" Snakefile.bak > Snakefile
+
 
 
 sudo docker build . -t szsctt/barcodes:latest -t szsctt/barcodes:3
