dc9a28a4cf175530c5a18b7ef027d5656d69f091
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2020-07-20 22:38:51 +1000

    removed sudo

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -14,4 +14,4 @@ perl -pe "s/src/\/src/g" Snakefile.bak > Snakefile
 
 
 
-sudo docker build . -t szsctt/barcodes:latest -t szsctt/barcodes:4
+docker build . -t szsctt/barcodes:latest -t szsctt/barcodes:4
